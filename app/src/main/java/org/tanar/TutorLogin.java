2cbc1079c36c81f02b390e881126440115bbc170
Marisa Raj <marisaraj14@gmail.com>
2021-04-01 05:08:15 +0530

    Tutor Design

---
+++ app/src/main/java/org/tanar/TutorLogin.java
@@ -0,0 +1,14 @@
+package org.tanar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TutorLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_login);
+    }
+}
